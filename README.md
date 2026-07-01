189e8287f70adfff2876c20634b8c3fdd59b87a5
adarsh <adarshmishr6@gmail.com>
2026-07-01 14:08:47 +0530

    Updated Fat jar config

--- README.md
+++ README.md
@@ -37,3 +37,29 @@ java -jar build/libs/filename.jar
 ````
 
 The above command executes the compiled jar file of the project
+
+**Notes**
+   - When we build jar from cli it by default does not create the fat jar (uber jar), means 
+     the created jar files contains only compiled .class files it not resolves external dependencies (external libraries). To handle this we should add Config in `build.gradle`
+     ```azure
+      jar {
+          duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+          manifest {
+             attributes(
+                 'Main-Class': 'your.package.MainClass'  
+             )
+          }
+    
+         from {
+             configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }
+         }
+     ```
+     
+   - Here `duplicatesStrategy = DuplicatesStrategy.EXCLUDE` tells Gradle Gradle to keep the first file it finds and skip any duplicates.
+   - And in line ` from {
+        configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }
+    }`
+   It Tells Gradle to Loop through all the dependencies in `build.gradle` and for each dependency if its an folder then leave as its 
+   else `zipTree(it)` **UnZip** that jar file down to its raw .class files and `from {...}` Takes all those unzipped class file from the respective links (eg: OkHttp) and 
+   copies them directly into the project's final JAR alongside your code
+   - This makes normal jar to fat jar files
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+        Request req = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response res = client.newCall(req).execute()) {
+              if (!res.isSuccessful())
+                  System.out.println("Something went wrong");
+
+            System.out.println(res.body().string());
         }
+        catch (IOException ex) {
+           ex.printStackTrace();
+        }
+
     }
 }
